f3c2c5b06edb7b99792cf4ae58ef338b7e63f17f
r <rrdlpld@github.com>
2023-01-15 18:09:27 +0100

    fixed color saga

--- lua/r/plugins/lsp/lspsaga.lua
+++ lua/r/plugins/lsp/lspsaga.lua
@@ -1,8 +1,19 @@
 -- import lspsaga safely
 local saga_status, saga = pcall(require, "lspsaga")
 if not saga_status then
-  return
+	return
 end
 
 saga.setup({
+	-- keybinds for navigation in lspsaga window
+	scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
+	-- use enter to open file with definition preview
+	definition = {
+		edit = "<CR>",
+	},
+	ui = {
+		colors = {
+			normal_bg = "#4d5154",
+		},
+	},
 })
