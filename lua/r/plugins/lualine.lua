0a94e343246cc4f7ef7527e8ee1a511e2d441c0d
r <rrdlpld@github.com>
2023-01-25 11:19:53 +0100

    added relative file path

--- lua/r/plugins/lualine.lua
+++ lua/r/plugins/lualine.lua
@@ -1,7 +1,7 @@
 -- import lualine plugin safely
 local status, lualine = pcall(require, "lualine")
 if not status then
-  return
+	return
 end
 
 -- get lualine nightfly theme
@@ -9,7 +9,16 @@ local lualine_molokai = require("lualine.themes.molokai")
 
 -- configure lualine with modified theme
 lualine.setup({
-  options = {
-    theme = lualine_molokai,
-  },
+	options = {
+		theme = lualine_molokai,
+	},
+	sections = {
+		lualine_c = {
+			{
+				"filename",
+				file_status = true, -- displays file status (readonly status, modified status)
+				path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+			},
+		},
+	},
 })
